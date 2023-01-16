c6385067b909e6aa23a396191e8dd7b431458380
Patricia Severance <patriciakseverance@gmail.com>
2023-01-16 16:11:26 -0700

    Add test cases for total_discounted_revenue instance method

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,16 +12,32 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :transactions}
   end
   describe "instance methods" do
-    it "total_revenue" do
+    before(:each) do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
       @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
       @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+      @discount1 = @merchant1.discounts.create!(quantity_threshold: 5, percent_discount: 50)
+      @discount2 = @merchant1.discounts.create!(quantity_threshold: 2, percent_discount: 10)
+    end
 
-      expect(@invoice_1.total_revenue).to eq(100)
+    it "total_revenue" do
+      expect(@invoice_1.total_revenue).to eq(110)
+    end
+
+    it '#total_discounted_revenue' do
+      expect(@invoice_1.total_discounted_revenue).to eq(65)
+
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 2, unit_price: 10, status: 1)
+      @invoice_1.reload
+
+      expect(@invoice_1.total_discounted_revenue).to eq(83)
+
+
     end
   end
 end
