d857e1fabbe9150d7e28d0581d149645df1a8b87
Patricia Severance <patriciakseverance@gmail.com>
2023-01-16 16:12:04 -0700

    Implement total_discounted_revenue instance method for invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,6 +7,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :completed]
 
@@ -14,7 +15,12 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-  def discounted_revenue
-    
+  def total_discounted_revenue
+    a = self.invoice_items.joins(item: [merchant: :discounts]).where('quantity_threshold <= quantity').group(:id).select('invoice_items.*, (100 - max(percent_discount)) * 0.01 as best_discount')
+
+    discounted_items_price = a.map{|row| row.quantity * row.unit_price * row.best_discount}.sum
+    undiscounted_items_price = self.invoice_items.joins(item: [merchant: :discounts]).where('? > quantity', self.discounts.minimum(:quantity_threshold)).group(:id).sum('quantity * items.unit_price')
+
+    discounted_items_price + undiscounted_items_price.values.sum
   end
 end
